42e0d33695ba974c5ff9953ff13f14a9fc6cf0b5
Shrouk Salah <shrouksalah707@outlook.com>
2026-09-17 23:55:28 +0300

    layout routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,53 +9,61 @@ import { NotFoundComponent } from './features/not-found/not-found.component';
 import { NotificationsComponent } from './features/notifications/notifications.component';
 import { ChangePasswordComponent } from './features/change-password/change-password.component';
 
-export const routes: Routes = [{
-    path: 'auth',
-    component: AuthLayoutComponent,
-    children: [{
-        path: 'login',
-        component: LoginComponent,
-        title: "Login page"
+export const routes: Routes = [
+    {
+        path: "",
+        redirectTo: 'login',
+        pathMatch: "full"
+    }, {
+        path: '',
+        component: AuthLayoutComponent,
+        children: [{
+            path: 'login',
+            component: LoginComponent,
+            title: "Login page"
+        },
+        {
+
+            path: 'register',
+            component: RegisterComponent,
+            title: "Register page"
+        }
+        ]
+    },
+    {
+        path: '',
+        component: MainLayoutComponent,
+        children: [{
+            path: 'feed',
+            component: FeedComponent,
+            title: "Feed page"
+        },
+        {
+
+            path: 'profile',
+            component: ProfileComponent,
+            title: "Profile page"
+        },
+
+        {
+
+            path: 'notifications',
+            component: NotificationsComponent,
+            title: "Not found page"
+        },
+        {
+
+            path: 'ChangePassword',
+            component: ChangePasswordComponent,
+            title: " Change Password page"
+        }
+        ]
     },
     {
 
-        path: 'register',
-        component: RegisterComponent,
-        title: "Register page"
-    }
-    ]
-},
-{
-    path: 'main',
-    component: MainLayoutComponent,
-    children: [{
-        path: 'feed',
-        component: FeedComponent,
-        title: "Feed page"
-    },
-    {
 
-        path: 'profile',
-        component: ProfileComponent,
-        title: "Profile page"
-    },
-    {
-
-        path: 'not-found',
+        path: '**',
         component: NotFoundComponent,
-        title: "Not found page"
-    },
-    {
+        title: "Page Not Found"
 
-        path: 'notifications',
-        component: NotificationsComponent,
-        title: "Not found page"
-    },
-    {
-
-        path: 'ChangePassword',
-        component: ChangePasswordComponent,
-        title: " Change Password page"
-    }
-    ]
-}];
+    }];
