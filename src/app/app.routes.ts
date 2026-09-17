0847f0b1d29c08a19971282c2747f036f0ff8f4b
Shrouk Salah <shrouksalah707@outlook.com>
2026-09-17 20:54:45 +0300

    layout routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,61 @@
 import { Routes } from '@angular/router';
+import { AuthLayoutComponent } from './core/layouts/auth-layout/auth-layout.component';
+import { LoginComponent } from './core/auth/components/login/login.component';
+import { RegisterComponent } from './core/auth/components/register/register.component';
+import { MainLayoutComponent } from './core/layouts/main-layout/main-layout.component';
+import { FeedComponent } from './features/feed/feed.component';
+import { ProfileComponent } from './features/profile/profile.component';
+import { NotFoundComponent } from './features/not-found/not-found.component';
+import { NotificationsComponent } from './features/notifications/notifications.component';
+import { ChangePasswordComponent } from './features/change-password/change-password.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{
+    path: 'auth',
+    component: AuthLayoutComponent,
+    children: [{
+        path: 'login',
+        component: LoginComponent,
+        title: "Login page"
+    },
+    {
+
+        path: 'register',
+        component: RegisterComponent,
+        title: "Register page"
+    }
+    ]
+},
+{
+    path: 'main',
+    component: MainLayoutComponent,
+    children: [{
+        path: 'feed',
+        component: FeedComponent,
+        title: "Feed page"
+    },
+    {
+
+        path: 'profile',
+        component: ProfileComponent,
+        title: "Profile page"
+    },
+    {
+
+        path: 'not-found',
+        component: NotFoundComponent,
+        title: "Not found page"
+    },
+    {
+
+        path: 'notifications',
+        component: NotificationsComponent,
+        title: "Not found page"
+    },
+    {
+
+        path: 'ChangePassword',
+        component: ChangePasswordComponent,
+        title: " Change Password page"
+    }
+    ]
+}];
