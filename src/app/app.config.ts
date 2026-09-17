10c3a6370888ad32180d9f51cde570cb2bfda969
Shrouk Salah <shrouksalah707@outlook.com>
2026-09-17 19:14:17 +0300

    components and styles

---
+++ src/app/app.config.ts
@@ -0,0 +1,19 @@
+import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { provideRouter, withHashLocation, withInMemoryScrolling, withViewTransitions } from '@angular/router';
+
+import { routes } from './app.routes';
+import { provideHttpClient, withFetch } from '@angular/common/http';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideRouter(routes,
+      withInMemoryScrolling({ scrollPositionRestoration: 'top' }),
+      withViewTransitions({
+        skipInitialTransition: true
+      }),
+      withHashLocation()
+    ),
+    provideHttpClient(withFetch())
+  ]
+};
