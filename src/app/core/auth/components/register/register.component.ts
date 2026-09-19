2f5fc29818043b10086f609cc7b17eda55f734b5
Shrouk Salah <shrouksalah707@outlook.com>
2026-09-19 19:44:08 +0300

    register design

--- src/app/core/auth/components/register/register.component.ts
+++ src/app/core/auth/components/register/register.component.ts
@@ -1,9 +1,26 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css',
 })
-export class RegisterComponent {}
+export class RegisterComponent {
+  registerForm: FormGroup = new FormGroup({
+    name: new FormControl("", [Validators.required, Validators.minLength(3)]),
+    username: new FormControl(""),
+    email: new FormControl("", [Validators.required, Validators.email]),
+    dateOfBirth: new FormControl("", [Validators.required,]),
+    gender: new FormControl("", [Validators.required, Validators.minLength(3)]),
+    password: new FormControl("", [Validators.required, Validators.minLength(5), Validators.pattern(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$/)]),
+    rePassword: new FormControl("", [Validators.required, Validators.minLength(5)])
+  })
+  // check Password = rePassword
+
+  onSubmit() {
+     console.log(this.registerForm.value)
+  }
+}
+
