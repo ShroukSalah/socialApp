0028717c483aa1ef2c2a221c61f52ed75d689ec8
Shrouk Salah <shrouksalah707@outlook.com>
2026-09-20 22:41:40 +0300

    environments

--- src/app/core/auth/services/auth.service.ts
+++ src/app/core/auth/services/auth.service.ts
@@ -2,17 +2,18 @@ import { HttpClient } from '@angular/common/http';
 import { inject, Service } from '@angular/core';
 import { Observable } from 'rxjs';
 import { UserDataResponse } from '../models/user-data.interface';
+import { environment } from '../../../../environments/environment.development';
 
 @Service()
 export class AuthService {
     private readonly httpClient = inject(HttpClient)
 
     sendRegisterData(data: object): Observable<UserDataResponse> {
-        return this.httpClient.post<UserDataResponse>("https://route-posts.routemisr.com/users/signup", data)
+        return this.httpClient.post<UserDataResponse>(environment.base_url + "/users/signup", data)
     }
 
     sendLoginData(data: object): Observable<UserDataResponse> {
-        return this.httpClient.post<UserDataResponse>("https://route-posts.routemisr.com/users/signin", data)
+        return this.httpClient.post<UserDataResponse>(environment.base_url + "/users/signin", data)
     }
 
 }
